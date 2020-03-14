ea5027cc7bf005ff375cff54f7eda09decccac51
Adam Wiśniewski <adamwisiwmada@gmail.com>
2020-03-14 15:39:29 +0100

    Let's fix this

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.ComponentModel;
-using System.Numerics;
+﻿using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -13,33 +11,11 @@ namespace Fractals {
 	public partial class MainWindow : Window {
 		public MainWindow() {
 			InitializeComponent();
-			bitmap = new WriteableBitmap(2000, 2000, 500, 500, PixelFormats.Bgra32, null);
-			pixels = new byte[2000, 2000, 4];
-		}
+			bitmap = new WriteableBitmap(2000, 2000, 500d, 500d, PixelFormats.Bgra32, null);
+			pixels = new byte[bitmap.PixelWidth, bitmap.PixelHeight, 4];
+			height = bitmap.PixelHeight;
+			width = bitmap.PixelWidth; //wymiary bitmapy
 
-		WriteableBitmap bitmap;
-		byte[,,] pixels;
-		Window1 pgWindow;
-		BackgroundWorker calcWorker = null;
-
-		int checkConvergence(Complex c, int acc) {
-			Complex z = Complex.Zero;   //z0
-			int color = 0;
-			for(int i = 0; i < acc; i++) {
-				if (z.Magnitude >= 2) {
-					return 0;
-				}
-				z = Complex.Pow(z, 2);
-				z += c;                 //zN+1 = zN^2 + C 
-				color++;
-			}
-			return color;
-		}
-
-		private void Button_Click(object sender, RoutedEventArgs e) {
-			pgWindow = new Window1();
-			pgWindow.pgBar.Value = 0;
-			pgWindow.Show();
 			if (calcWorker == null) {
 				calcWorker = new BackgroundWorker();
 				calcWorker.DoWork += new DoWorkEventHandler(calcWorker_DoWork);
@@ -47,26 +23,61 @@ namespace Fractals {
 				calcWorker.ProgressChanged += new ProgressChangedEventHandler(calcWorker_ProgressChanged);
 				calcWorker.WorkerReportsProgress = true;
 				calcWorker.WorkerSupportsCancellation = true;
-				if (!calcWorker.IsBusy) {
-					calcWorker.RunWorkerAsync();
-				}
 			}
 		}
 
+
+		WriteableBitmap bitmap;
+		int width, height;
+		byte[,,] pixels;
+		Window1 pgWindow;
+		BackgroundWorker calcWorker = null;
+
+		byte checkConvergence(float[] c, int acc) {
+			float[] z = { 0, 0 };       //z0
+			for (int i = 0; i < acc; i++) {
+				if (z[0] * z[0] + z[1] * z[1] > 4) {
+					return 0;
+				}
+				float[] tmp = new float[2];
+				tmp[0] = z[0];
+				tmp[1] = z[1];
+				//bo Zuze
+				z[0] = tmp[0] * tmp[0] - tmp[1] * tmp[1] + c[0];
+				z[1] = 2 * tmp[0] * tmp[1] + c[1];      //zN+1 = zN^2 + c
+			}
+			return 0xFF;
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e) {
+			pgWindow = new Window1();
+			pgWindow.pgBar.Value = 0;
+			pgWindow.Show();
+
+			if (!calcWorker.IsBusy) {
+				calcWorker.RunWorkerAsync();
+			}
+
+		}
+
 		private void calcWorker_DoWork(object sender, DoWorkEventArgs e) {
-			int height = 2000, width = 2000;	//wymiary bitmapy
 			int count = 0;
 			for (int row = 0; row < height; row++) {
 				for (int col = 0; col < width; col++) {
-					for (int i = 0; i < 3; i++) pixels[row, col, i] = 0;	//R, G i B na 0
+					for (int i = 0; i < 3; i++) pixels[row, col, i] = 0;    //R, G i B na 0
+
+					float[] z = new float[2];
+					z[0] = (float)(col - 3 * width / 4) / 900f;
+					z[1] = (float)(row - height / 2) / 900f;
+
+					pixels[row, col, 3] = (byte)checkConvergence(z, 100);           //nadaje kolor (alpha) w zaleznosci od 
+																					//ilosci iteracji (chyba)
+
 					count++;
-					Complex z = new Complex((float)(col - 1650) / 900f, (float)(row - 1050) / 900f);
-					if (checkConvergence(z, 100)) {
-						pixels[row, col, 3] = 255;
-					} else { pixels[row, col, 3] = 0; }
 					calcWorker.ReportProgress((int)(100 * count / (float)(width * height)));
 				}
 			}
+			calcWorker.ReportProgress(100);
 		}
 
 		private void calcWorker_ProgressChanged(object sender, ProgressChangedEventArgs e) {
@@ -75,26 +86,27 @@ namespace Fractals {
 
 		private void calcWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
 			pgWindow.Close();
-			byte[] pixels1d = new byte[2000 * 2000 * 4];
+			byte[] pixels1d = new byte[width * height * 4];
 			int index = 0;
-			for (int row = 0; row < 2000; row++) {
-				for (int col = 0; col < 2000; col++) {
+			for (int row = 0; row < height; row++) {
+				for (int col = 0; col < width; col++) {
 					for (int i = 0; i < 4; i++)
 						pixels1d[index++] = pixels[row, col, i];
 				}
 			}
 
-			Int32Rect rect = new Int32Rect(0, 0, 2000, 2000);
-			int stride = 4 * 2000;
+			Int32Rect rect = new Int32Rect(0, 0, width, height);
+			int stride = 4 * width;
 			bitmap.WritePixels(rect, pixels1d, stride, 0);
 
 			Image image = new Image() {
 				Source = bitmap,
 				Stretch = Stretch.None,
-				Margin = new Thickness(0)
+				Margin = new Thickness(0),
+				Width = 790,
+				Height = 718
 			};
 			canvas.Children.Add(image);
 		}
-
 	}
 }
